401b7bcf9639ef23fbe26acb06c41aeb702829b2
Tyisha <118560713+tyisha-A@users.noreply.github.com>
2024-11-25 12:38:04 -0500

    Class and main skeleton created

--- objectOriented.py
+++ objectOriented.py
@@ -1,3 +1,18 @@
 #This will be a object oriented version of the virtual3d game.
 
+class faceFinder:
+	"""Uses Haar Cascade filter to detect largest face from a frame."""
+	def init(self):
+		print("Face Finder initialize")
+
+#---------------------------------------------------------------------
+#Main
+#
+
 print("Starting O.O Virtual3d")
+
+
+ff = faceFinder()
+print("Virtual3d complete.")
+
+
